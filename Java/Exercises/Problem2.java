23926ed53fdb88d3403d3873e24836614c9cd41c
ColtenHargett <chargett@loyola.edu>
2026-02-12 11:31:13 -0500

    Update Problem2.java

--- Java/Exercises/Problem2.java
+++ Java/Exercises/Problem2.java
@@ -1,3 +1,11 @@
+/*
+Task:
+Write a program that counts down from 30 to 1. But:
+If the number is divisible by 3, print "Beep" after the number
+If the number is divisible by 5, print "Boop" after the number
+If divisible by both, print "BeepBoop" after the number
+Otherwise, just print the number
+*/
 import java.util.Scanner;
 
 public class Problem2 {
